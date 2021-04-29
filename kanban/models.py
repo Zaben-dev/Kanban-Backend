5017ce605744b3b3be9f9a5ce98b2a5de276dc5a
JerzyDeb <jerzydebowski.kontakt@gmail.com>
2021-04-29 10:32:46 +0200

    Position in column and row

--- kanban/models.py
+++ kanban/models.py
@@ -6,7 +6,9 @@ def checkPositions(col):
     print('-----col = %s-----'%col)
 
     countCols = Columns.objects.filter().count() #Number of columns
+    countRows = Rows.objects.filter().count() #Number of rows
     colList = [] #array of columns
+    rowList = [] #array of rows
 
     if col > countCols-1:#If col is < than number of columns - End of function
         return 0
@@ -14,47 +16,53 @@ def checkPositions(col):
     for i in range(countCols): #Filling the columns array
         colList.append(Columns.objects.filter()[i:i+1].first()) 
 
+    for i in range(countRows): #Filling the rows array
+        rowList.append(Rows.objects.filter()[i:i+1].first()) 
+
     print('-----For column: %s-----'%colList[col])
 
-    count = Tasks.objects.filter(column=colList[col]).count()#Number of tasks in current column
-    tasList = [] #array of tasks
+    for i in range(countRows):
+        print('i=%s'%(i))
+        count = Tasks.objects.filter(column=colList[col],row=rowList[i]).count()#Number of tasks in current column and row
+        tasList = [] #array of tasks
 
-    for j in range(count): 
-        tasList.append(Tasks.objects.filter(column=colList[col])[j:j+1].first()) #Filling the tasks array
+        for j in range(count): 
+            tasList.append(Tasks.objects.filter(column=colList[col],row=rowList[i])[j:j+1].first()) #Filling the tasks array
 
-    print('Number of tasks in col %s = %s'%(colList[col],count))
+        print('Number of tasks in col %s and row %s = %s'%(colList[col],rowList[i],count))
     
-    min = Tasks.objects.filter(column=colList[col]).aggregate(smallest=models.Min('position'))['smallest'] #Find a minimum position in column
+        min = Tasks.objects.filter(column=colList[col],row=rowList[i]).aggregate(smallest=models.Min('position'))['smallest'] #Find a minimum position in column
 
-    print('Min position in col %s = %s'%(colList[col],min))
+        print('Min position in col %s and row %s = %s'%(colList[col],rowList[i],min))
 
-    if min is None: #If min is none - Column is empty - Change to the next column (col=col+1)
-        print('Komórka pusta - Zmieniono komórke')
-        checkPositions(col+1)
+        if min is None: #If min is none - Column is empty - Change to the next column (col=col+1)
+            print('Komórka pusta - Zmieniono komórke')
+            checkPositions(col+1)
+            
 
-    if len(tasList) == 1: #If number of tasks on column is one - Change to the next column (col=col+1)
-        print('One task in col - column has been changed')
-        checkPositions(col+1)
+        if len(tasList) == 1: #If number of tasks on column is one - Change to the next column (col=col+1)
+            print('One task in col - column has been changed')
+            checkPositions(col+1)
     
-    if min != 1 and min is not None: #If min exists and min is not 1 - Change minimum to one
-        with transaction.atomic():
-            for task in Tasks.objects.filter(column=colList[col], position=min):
-                min = 1
-                task.position = min
-                print('Minimum value wasn`t `1` - Minimum was changed to `1`')
-                task.save(col=1)
+        if min != 1 and min is not None: #If min exists and min is not 1 - Change minimum to one
+            with transaction.atomic():
+                for task in Tasks.objects.filter(column=colList[col],row=rowList[i], position=min):
+                    min = 1
+                    task.position = min
+                    print('Minimum value wasn`t `1` - Minimum was changed to `1`')
+                    task.save(col=1)
 
-    if min == 1: #If min is 1 - Column is not empty - Check positions
-        for l in range (len(tasList)-1):
-            if(tasList[l+1].position-tasList[l].position != 1):
-                with transaction.atomic():
-                    for task in Tasks.objects.filter(col=colList[col],position=tasList[l+1].position):
-                        task.position = 1+tasList[l].position
-                        task.save(col=1)
-                        print('Changed position of task (id:%s) from |%s| to |%s|'%(tasList[l+1].id,tasList[l+1].position,1+tasList[l].position))
-                        tasList[l+1].position = 1+tasList[l].position 
-        print('Column has been changed')
-        checkPositions(col+1)
+        if min == 1: #If min is 1 - Column is not empty - Check positions
+            for l in range (len(tasList)-1):
+                if(tasList[l+1].position-tasList[l].position != 1):
+                    with transaction.atomic():
+                        for task in Tasks.objects.filter(col=colList[col],row=rowList[i],position=tasList[l+1].position):
+                            task.position = 1+tasList[l].position
+                            task.save(col=1)
+                            print('Changed position of task (id:%s) from |%s| to |%s|'%(tasList[l+1].id,tasList[l+1].position,1+tasList[l].position))
+                            tasList[l+1].position = 1+tasList[l].position 
+            print('Column has been changed')
+            checkPositions(col+1)
         
 
 
@@ -153,9 +161,9 @@ class Tasks(models.Model):
         countCols = Columns.objects.filter().count()
         zmiana = True
         while zmiana:
-            if Tasks.objects.filter(column=self.column,position=self.position).exists():
+            if Tasks.objects.filter(column=self.column,row=self.row,position=self.position).exists():
                 with transaction.atomic():
-                    for task in Tasks.objects.filter(column=self.column,position=self.position):
+                    for task in Tasks.objects.filter(column=self.column,row=self.row,position=self.position):
                         task.position = task.position+1
                         task.save(col=1)
                         zmiana=True
@@ -173,6 +181,6 @@ class Tasks(models.Model):
         return self.title
 
     class Meta:
-        ordering = ['position']
+        ordering = ['row','position']
 
 
